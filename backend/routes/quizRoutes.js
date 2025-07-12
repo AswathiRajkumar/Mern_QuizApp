095f4eff6a3a162ec2a8f71f0958d557b9349cff
Aswathi R <143251872+AswathiRajkumar@users.noreply.github.com>
2025-07-12 08:54:51 +0530

    Update quizRoutes.js

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Question = require("../models/Question");
-
-// GET all questions
 router.get("/", async (req, res) => {
   try {
     const questions = await Question.find();
